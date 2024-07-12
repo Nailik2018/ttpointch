328a955d03e2ba3f029786e46136cc32fadc3031
= <kilualthaus@hotmail.com>
2024-07-12 15:21:29 +0200

    env, swagger, db, custom logger, eslinter, delete app service und controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AssociationModule} from './module/association/association.module';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('MYSQL_HOST'),
+        port: configService.get<number>('MYSQL_PORT'),
+        username: configService.get<string>('MYSQL_USER'),
+        password: configService.get<string>('MYSQL_PASSWORD'),
+        database: configService.get<string>('MYSQL_DATABASE'),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: true,}),
+      inject: [ConfigService],
+    }),
+    AssociationModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {VersioningType} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
+import {AppCustomLogger} from './app.custom.logger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
+
+  const logger: AppCustomLogger = new AppCustomLogger("Main");
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  logger.log("Application is starting");
+  const configService = app.get(ConfigService);
+  // cors
+  app.enableCors();
+  app.setGlobalPrefix("api");
+  // Version
+  app.enableVersioning({
+    type: VersioningType.URI
+  });
+  const config = new DocumentBuilder()
+    .setTitle("Nest.js API Screenshot Job Backend")
+    .setDescription("API documentation for Nest.js application with Swagger UI")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(configService.get("BACKEND_SWAGGER_PATH"), app, document);
+  await app.listen(configService.get("BACKEND_PORT") || 3001);
 }
+
 bootstrap();
